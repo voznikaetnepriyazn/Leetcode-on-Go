ab740b21f7e31ac74238e70cbf5b6e75b9939f89
Daria Maslova <maslovadaria16.07@gmail.com>
2026-01-20 21:15:06 +0300

    tasks

---
+++ package leetcode.go
@@ -0,0 +1,76 @@
+package leetcode
+
+import "strconv"
+
+func Num1413(nums []int) int {
+	startValue := 1
+	total := startValue
+	for _, i := range nums {
+		total += i
+		if total < 1 {
+			break
+		}
+	}
+}
+
+func Num119(row int, col int) int {
+	if row == 0 || col == 0 || row == col {
+		return 1
+	}
+
+	return Num119(row-1, col-1) + Num119(row-1, col)
+}
+
+func getRow119(rowIndex int) []int {
+	ans := make([]int, rowIndex+1)
+	for i := 0; i <= rowIndex; i++ {
+		ans[i] = Num119(rowIndex, i)
+	}
+	return ans
+}
+
+func Num1009(num int) int64 {
+	bin := strconv.FormatInt(int64(num), 2)
+	for _, i := range bin {
+		if i == 0 {
+			i = 1
+		} else if i == 1 {
+			i = 0
+		}
+	}
+	res, err := strconv.ParseInt(str, 2, 64)
+	return res
+}
+
+func Num1150(nums []int, target int) bool {
+	for i := 0; i < len(nums); i++ {
+		count := 0
+		if i != target {
+			return false
+
+		} else if i == target {
+			count++
+		}
+
+		if count <= len(nums)/2 {
+			return false
+		}
+	}
+	return true
+}
+
+func Num830(s string) [][]int {
+	var res [][]int
+	i := 0
+
+	for j := 0; j < len(s); j++ {
+		if s[j] != s[j+1] || j == len(s)-1 {
+			if j-i+1 >= 3 {
+				res = append(res, []int{i, j})
+			}
+
+			i = j + 1
+		}
+	}
+	return res
+}
